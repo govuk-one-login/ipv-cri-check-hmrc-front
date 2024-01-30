ad77e6db8e2dae037d0c240c65b3b0833c2f068f
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2024-01-30 09:44:53 +0000

    test: exclude form-wizard config and assets from test coverage calculation

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,3 @@
-const wipCoveragePathIgnorePatterns = [
-  "src/app.js",
-  "src/assets/.*",
-  "src/app/.*/index.js",
-];
-
 module.exports = {
   clearMocks: true,
   modulePaths: ["<rootDir>/src"],
@@ -11,10 +5,12 @@ module.exports = {
   testMatch: ["<rootDir>/tests/unit/**/*.test.js"],
   setupFilesAfterEnv: ["<rootDir>/tests/unit/lib/helpers"],
   coveragePathIgnorePatterns: [
-    "./src/app/.*/steps.js",
-    "./src/app/.*/fields.js",
-    "./src/lib/config.js",
-  ].concat(wipCoveragePathIgnorePatterns),
+    "src/assets/.*",
+    "src/app/.*/fields.js",
+    "src/app/.*/steps.js",
+    "src/app/.*/index.js",
+    "src/app.js",
+  ],
   coverageThreshold: {
     global: {
       statements: 100,
