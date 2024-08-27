bde5f4d99c3058036d4ebe7a7277f1b26e010d6a
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2024-08-27 15:45:59 +0100

    OJ-2752: Testing post merge Dockerfile

--- tests/browser/run-tests.sh
+++ tests/browser/run-tests.sh
@@ -3,5 +3,6 @@
 export USE_LOCAL_API="false"
 export WEBSITE_HOST="https://review-hc.dev.account.gov.uk/"
 export ENV="dev"
+export RELYING_PARTY_URL=$(aws ssm get-parameter --name "/check-hmrc-cri-api/smoke-tests/core-stub-url" --query "Parameter.Value" --output text)
 
 npm run test:browser -- --tags @post-merge
