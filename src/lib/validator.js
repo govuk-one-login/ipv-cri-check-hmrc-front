dcc8af642b5289147114652703e5e317a5bc556b
MarieseMikely <mariese.mikely@digital.cabinet-office.gov.uk>
2025-02-18 11:13:46 +0000

    OJ-3007: fix - updated validation file to return a boolean based on GH suggession and removed if statements that are no longer needed

--- src/lib/validator.js
+++ src/lib/validator.js
@@ -1,34 +1,24 @@
 const isValidNino = (value) => {
   const formattedValue = value.replaceAll(" ", "").toUpperCase();
-  if (formattedValue.length < 9 || formattedValue.length > 9) {
-    return null;
-  }
+
   const formatRegex = /^[A-Z]{2}\d{6}[A-Z]$/i;
-  if (!formatRegex.test(formattedValue)) {
-    return null;
-  }
-  return formattedValue;
+  return formatRegex.test(formattedValue);
 };
 
 const invalidCharacters = (value) => {
   const formattedValue = value.replaceAll(" ", "").toUpperCase();
-  if (isValidNino(formattedValue) !== formattedValue) {
-    return formattedValue;
-  }
+
   const prefixRegex = /^(?:BG|GB|KN|NK|NT|TN|ZZ)/;
   const firstLetterRegex = /^[DFIQUV][A-Z]/;
   const secondLetterRegex = /^[A-Z][DFIQUVO]/;
   const lastLetterRegex = /[A-Z]{2}\d{6}[^ABCD]$/;
 
-  if (
+  return !(
     prefixRegex.test(formattedValue) ||
     firstLetterRegex.test(formattedValue) ||
     secondLetterRegex.test(formattedValue) ||
     lastLetterRegex.test(formattedValue)
-  ) {
-    return null;
-  }
-  return formattedValue;
+  );
 };
 
 module.exports = { isValidNino, invalidCharacters };
