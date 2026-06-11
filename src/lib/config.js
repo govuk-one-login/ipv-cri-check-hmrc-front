79dafbc44b17cfa6f670f88255d2084ba7f1cd12
Mike Beeby <mike.beeby@digital.cabinet-office.gov.uk>
2026-06-11 13:15:19 +0100

    OJ-3720: Remove old feature flags

--- src/lib/config.js
+++ src/lib/config.js
@@ -17,16 +17,9 @@ module.exports = {
       CHECK: "/",
     },
     GTM: {
-      UA_ENABLED: process.env.UA_ENABLED || "false",
-      UA_CONTAINER_ID: process.env.UA_CONTAINER_ID,
-      GA4_ENABLED: process.env.GA4_ENABLED || "false",
       GA4_CONTAINER_ID: process.env.GA4_CONTAINER_ID,
-      ANALYTICS_DATA_SENSITIVE: process.env.ANALYTICS_DATA_SENSITIVE,
     },
     FRONTEND_DOMAIN: process.env.FRONTEND_DOMAIN || "localhost",
-    LANGUAGE_TOGGLE_DISABLED: process.env.LANGUAGE_TOGGLE_DISABLED || "true",
-    DEVICE_INTELLIGENCE_ENABLED:
-      process.env.DEVICE_INTELLIGENCE_ENABLED || "false",
     DEVICE_INTELLIGENCE_DOMAIN:
       process.env.DEVICE_INTELLIGENCE_DOMAIN || "account.gov.uk",
   },
